bdaa1186b542b1a22c6d04be68e603e2175bdf79
afikri <fikri.abdul@gmail.com>
2022-12-12 00:07:50 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Card, Sidebar } from "./components/";
+import Dashboard from "./pages/Dashboard";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen">
+      <Sidebar />
+      <div class="w-full px-4 py-2 bg-gray-200 lg:w-full">
+        <div class="container mx-auto mt-12">
+          <Card />
+          <Dashboard />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
